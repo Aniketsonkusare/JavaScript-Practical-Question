aef608602c8adb1e6e24d394422b8bf78df5d864
Aniketsonkusare <aniketsonkusare70@gmail.com>
2024-02-05 09:31:59 +0530

    isInt-or-not

---
+++ 4-isInt.js
@@ -0,0 +1,19 @@
+let a = 12.1;
+ 
+
+if (a % 1 === 0) {
+    console.log("this is integer - ",a);
+}
+else{
+    console.log("this is a not integer -",a);
+}
+
+
+let b = 21
+
+if (b % 1 === 0) {
+    console.log("This is integer -",b);
+}
+else{
+    console.log("this is not integer - ",b);
+}
